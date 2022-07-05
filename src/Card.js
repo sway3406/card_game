bfcbe80a02e128115eac78475527516bb3daadec
Shane Way <shane.c.way2@gmail.com>
2022-07-05 17:39:48 -0600

    This commit completes the Card.js class

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+const data = require('./data');
+const prototypeQuestions = data.prototypeData;
+const util = require('./util');
+
+class Card {
+    constructor(id, question, answers, correctAnswer ) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    }
+}
+
+module.exports = Card;
