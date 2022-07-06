d692d81b4fa4e7b32dd3dca8beb3d513ecf05d3a
Shane Way <shane.c.way2@gmail.com>
2022-07-06 17:35:09 -0600

    This commit completes Turn.js implementation code

--- src/Turn.js
+++ src/Turn.js
@@ -3,7 +3,30 @@ const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Turn {
-    
+    constructor(userGuess, card) {
+        this.userGuess = userGuess;
+        this.card = card;
+    }
+    returnGuess() {
+        return this.userGuess;
+    }
+    returnCard() {
+        return this.card;
+    }
+    evaluateGuess() {
+        if (this.userGuess !== this.card.correctAnswer) {
+            return false;
+        } else {
+            return true;
+        } 
+    }
+    giveFeedback(){
+        if(this.evaluateGuess() === true) {
+            return 'correct!';
+        } else {
+            return 'incorrect';
+        }
+    }
 }
 
 module.exports = Turn;
