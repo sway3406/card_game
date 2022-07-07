a78271ed550aa675db2cd7eb37973829aa3f3f04
Shane Way <shane.c.way2@gmail.com>
2022-07-06 20:44:33 -0600

    This commit adds the Deck class and countCards method

--- src/Deck.js
+++ src/Deck.js
@@ -1,9 +1,15 @@
+const Card = require('./Card');
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Deck {
-    
+    constructor(cardDeck) {
+        this.cardDeck = cardDeck;
+    }
+    countCards() {
+        return this.cardDeck.length;
+    }
 }
 
 module.exports = Deck;
