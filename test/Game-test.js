c86b20cd02c834eaf9ac14a675f6898af67cb8cd
Shane Way <shane.c.way2@gmail.com>
2022-07-08 18:11:31 -0600

    This commit completes the passes all test in Game-test suite

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,7 +1,34 @@
 const chai = require('chai');
 const expect = chai.expect;
-const Card = require('../src/Card');
-const Deck = require('../src/Deck');
-const Turn = require('../src/Turn');
-const Round = require('../src/Round');
-// const { prototypeData } = require('../src/data');
+const Game = require('../src/Game');
+
+
+
+describe('Game', () => {
+    let game;
+
+    beforeEach( () => {
+    game = new Game();
+
+    });
+
+    it('should be a function', () => {
+        expect(Game).to.be.a('function');
+    });
+
+
+    it('should create new cards', () => {
+
+        expect(game.createCards()).to.be.an('array');
+    });
+
+    it('should put cards in a deck', () => {
+
+        expect(game.placeCardsInDeck()).to.be.an('object');
+    });
+
+    it('should create a new round using the deck', () => {
+
+        expect(game.createNewRound()).to.be.an('object');
+    });
+});
