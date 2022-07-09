39d9a75b6291e9794bde48619df1bf5430f7c927
Shane Way <shane.c.way2@gmail.com>
2022-07-08 18:13:50 -0600

    This commit completes the Game.ja implementation code file

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,17 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
-
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null;
+    this.cardDeck = null
+  }
+
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -17,6 +20,23 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+  createCards() {
+    let cards = prototypeQuestions.map((newCard) => {
+        return new Card(newCard.id, newCard.question,newCard.answers,newCard.correctAnswer)
+      }); 
+      return cards;
+  }
+  placeCardsInDeck() {
+    return this.cardDeck = new Deck(this.createCards());
+  }
+  createNewRound() {
+    return this.currentRound = new Round(this.placeCardsInDeck());
+  }
+  startGame() {
+    this.printMessage(this.placeCardsInDeck(), this.createNewRound());
+    this.printQuestion(this.createNewRound());
+    this.createNewRound();
   }
 }
 
